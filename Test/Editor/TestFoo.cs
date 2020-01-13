d44ed6df47a2b2e0b13c99c251c91e748887a370
Robin Pan <rpan@eastsidegamestudio.com>
2020-01-13 10:50:49 -0800

    Update asmdef

--- Test/Editor/TestFoo.cs
+++ Test/Editor/TestFoo.cs
@@ -1,11 +1,7 @@
-fileFormatVersion: 2
-guid: 9916795889f8b4be5a8d7206ede28f34
-MonoImporter:
-  externalObjects: {}
-  serializedVersion: 2
-  defaultReferences: []
-  executionOrder: 0
-  icon: {instanceID: 0}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 
+namespace npm.test
+{
+   public class TestFoo : Foo
+   {
+
+   }
+}
